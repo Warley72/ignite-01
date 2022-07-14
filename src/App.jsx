367515189997e9fc5a2772deeeb70f8ce332fd5c
carlos warley <carloswarley0@gmail.com>
2022-07-14 00:02:54 -0300

    CSS global

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
 
+import './global.css';
 
 function App() {
   
   return (
-    <h1>HELLO WORLD</h1>
+    <>
+      <Header />
+
+      <Post 
+        author="carlos"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum, 
+          ducimus possimus repudiandae expedita obcaecati fugit quia fuga quos praesentium 
+          quidem harum aspernatur veniam veritatis quasi a consequuntur, vitae, iusto numquam?"
+      />
+      <Post
+        author="warley"
+        content="o verdadeiro programador, ele programa a Dor "
+      />
+    </>
   )
 }
 
 export default App
+
